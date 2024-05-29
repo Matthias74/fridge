f211d72a8d21bee574218bd1a7bb07d90a5a2592
Matthias Vitte <m.vitte@stuart.com>
2024-05-29 10:19:24 +0200

    create seeds

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "vite_rails"
 gem "jbuilder"
 gem "redis", "~> 4.0"
 gem 'bootstrap', '~> 5.2.0'
+gem "activerecord-import"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+valid_recipe_datas = []
+
+def ingredient_attributes(ingredient)
+  pattern = /^((.*?)\s(de|d')|(\d\s)|1\/\d)/
+  match = pattern.match(ingredient)
+  name = match ? ingredient.gsub(pattern, "").squish : ingredient
+  {
+    name: name.downcase,
+    quantity: match ? match[0].squish : nil
+  }
+end
+
+def has_invalid_ingredients?(ingredients)
+  varchar_max_length = 255
+  ingredients.pluck(:name).detect { |n| n.length > varchar_max_length }
+end
+
+File.readlines("#{Rails.root}/tmp/recipes.json").each do |line|
+  begin
+    recipe_data = JSON.parse(line)
+
+    recipe[:ingredients_attributes] = recipe_data["ingredients"].map do |ingredient|
+      ingredient_attributes(ingredient)
+    end
+
+    next if has_invalid_ingredients?(recipe[:ingredients_attributes])
+
+    valid_recipe_datas << recipe_data
+  rescue JSON::ParserError
+    next
+  end
+end
+
+recipes = []
+ingredients = valid_recipe_datas.pluck(:ingredients_attributes).flatten.uniq.map do |ingredient_data|
+  Ingredient.new(ingredient_data)
+end
+
+valid_recipe_datas.each do |recipe_data|
+  recipes << Recipe.new({
+    rate: recipe_data["rate"]&.to_i,
+    author_tip: recipe_data["author_tip"],
+    budget: recipe_data["budget"],
+    prep_time: recipe_data["prep_time"],
+    name: recipe_data["name"],
+    difficulty: recipe_data["difficulty"],
+    people_quantity: recipe_data["people_quantity"]&.to_i,
+    cook_time: recipe_data["cook_time"],
+    total_time: recipe_data["total_time"],
+    nb_comments: recipe_data["nb_comments"],
+  })
+end
