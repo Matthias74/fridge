48903fcbce99057bb10fd25a5c63e5286ec0aaa9
Matthias Vitte <m.vitte@stuart.com>
2024-06-04 18:33:38 +0200

    fill modal and pagination

--- app/javascript/views/recipe_modal.vue
+++ app/javascript/views/recipe_modal.vue
@@ -18,18 +18,49 @@
 </script>
 <template>
     <div ref="myModal" class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
             <h1 class="modal-title fs-5" id="staticBackdropLabel">{{recipe.name}}</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            ...
+            <div class="d-flex justify-content-between">
+              <span>
+                <span class="fw-semibold">Budget:</span>
+                {{ recipe.budget || '- '}}
+              </span>
+              <span>
+                <span class="fw-semibold">Cook Time:</span>
+                {{ recipe.cook_time || '-' }}
+              </span>
+              <span>
+                <span class="fw-semibold">Difficulty:</span>
+                {{ recipe.difficulty || '-' }}
+              </span>
+              <span>
+                <span class="fw-semibold">People quantity:</span>
+                {{ recipe.people_quantity || '-' }}
+              </span>
+              <span>
+                <span class="fw-semibold">Preparation Time:</span>
+                {{ recipe.prep_time || '-' }}
+              </span>
+            </div>
+            <div class="mt-5">
+              <h2 class="fs-5">Ingredients</h2>
+              <ul>
+                <li v-for="ingredient in recipe.ingredients">
+                  <span v-if="ingredient.quantity">
+                    {{ ingredient.quantity + " "}}
+                  </span>
+                  <span>{{ ingredient.name }}</span>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
           </div>
         </div>
       </div>
--- app/javascript/views/recipes.vue
+++ app/javascript/views/recipes.vue
@@ -12,6 +12,11 @@
         ingredients: [],
         ingredient: null,
         recipe: null,
+        isFetching: false,
+        pagination: {
+          currentPage: null,
+          totalPages: null,
+        }
       }
     },
     methods: {
@@ -24,16 +29,29 @@
         this.ingredients = this.ingredients.filter(i => i !== ingredient)
       },
       findRecipes() {
+        this.isFetching = true
         axios.get(`/api/recipes`, {
-					params: { ingredients: this.ingredients }
+					params: { ingredients: this.ingredients, page: this.pagination.currentPage }
 				})
 				.then(function (response) {
+          this.isFetching = false
 					this.recipes = response.data.recipes
+          this.pagination.currentPage = response.data.pagination.current_page
+          this.pagination.totalPages = response.data.pagination.total_pages
+          console.log(this.pagination)
 				}.bind(this))
       },
       displayeRecipe(recipe){
         this.recipe = recipe
         this.openModal()
+      },
+      nextPage() {
+        this.pagination.currentPage += 1
+        this.findRecipes()
+      },
+      previousPage() {
+        this.pagination.currentPage -= 1
+        this.findRecipes()
       },
       showModal(modalComponent, data) {
 	      const element = document.createElement("div")
@@ -110,20 +128,50 @@
             Find recipes
           </button>
         </div>
-
-        <h4 class="text-center">Recipes:</h4>
-        <ul v-if="recipes.length">
-          <li
-            v-for="recipe in recipes" :key="recipe.id"
-            class="mt-2"
-          >
-            {{ recipe.name }}
-            <span 
-              @click="displayeRecipe(recipe)"
-              class="btn btn-sm btn-primary">Display</span>
-          </li>
-        </ul>
-        <p v-else>No recipes found</p>
+        <template v-if="isFetching">
+          <div class="text-center mt-5">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <h4 class="text-center">Recipes:</h4>
+          <template v-if="recipes.length">
+            <ul>
+              <li
+                v-for="recipe in recipes" :key="recipe.id"
+                class="mt-2"
+              >
+                {{ recipe.name }}
+                <span
+                  @click="displayeRecipe(recipe)"
+                  class="btn btn-sm btn-primary">Display</span>
+              </li>
+            </ul>
+            <ul class="pagination">
+              <li
+                :class="{'disabled': pagination.currentPage === 1}"
+                class="page-item">
+                <a
+                  @click.stop.prevent="previousPage()"
+                  class="page-link" href="#" tabindex="-1">
+                    Previous
+                </a>
+              </li>
+              <li
+                :class="{'disabled': pagination.currentPage === pagination.totalPages }"
+                class="page-item">
+                <a
+                  @click.stop.prevent="nextPage()"
+                  class="page-link" href="#" tabindex="-1">
+                    Next
+                </a>
+              </li>
+            </ul>
+          </template>
+          <p v-else>No recipes found</p>
+        </template>
       </div>
     </div>
   </div>
